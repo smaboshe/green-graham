c6176d69c5a5db4e73b568c11a48591c713c4215
Silumesii Maboshe <silumesii@gmail.com>
2025-11-06 11:58:53 +0200

    Add test environment variables

---
+++ src/env.d.ts
@@ -0,0 +1,10 @@
+// <reference types="astro/client">
+interface ImportMetaEnv {
+  readonly PUBLIC_TEST_CILANTRO_URL: string;
+  readonly TEST_ACCOUNT_ID: string;
+  readonly TEST_SECRET_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
