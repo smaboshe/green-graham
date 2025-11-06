a8faa6105df2ea7ea5394ffc74e83c2ac763fdf2
Silumesii Maboshe <silumesii@gmail.com>
2025-11-06 13:19:37 +0200

    Variable renaming

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,8 +2,8 @@
 interface ImportMetaEnv {
   readonly PUBLIC_TEST_CILANTRO_URL: string;
   readonly PUBLIC_TEST_OREGANO_URL: string;
-  readonly TEST_SECRET_ACCOUNT_ID: string;
-  readonly TEST_SECRET_API_KEY: string;
+  readonly SECRET_TEST_ACCOUNT_ID: string;
+  readonly SECRET_TEST_API_KEY: string;
 }
 
 interface ImportMeta {
--- src/pages/api/env.json.ts
+++ src/pages/api/env.json.ts
@@ -4,8 +4,8 @@ export const GET: APIRoute = async () => {
   const envVars = {
     PUBLIC_TEST_CILANTRO_URL: import.meta.env.PUBLIC_TEST_CILANTRO_URL || "not set",
     PUBLIC_TEST_OREGANO_URL: import.meta.env.PUBLIC_TEST_OREGANO_URL || "not set",
-    TEST_SECRET_ACCOUNT_ID: import.meta.env.TEST_SECRET_ACCOUNT_ID || "not set",
-    TEST_SECRET_API_KEY: import.meta.env.TEST_SECRET_API_KEY || "not set"
+    SECRET_TEST_ACCOUNT_ID: import.meta.env.SECRET_TEST_ACCOUNT_ID || "not set",
+    SECRET_TEST_API_KEY: import.meta.env.SECRET_TEST_API_KEY || "not set"
   };
 
   return new Response(JSON.stringify(envVars, null, 2), {
