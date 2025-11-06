cd7ea259e8f71a24c698574da955db2b3909ffe9
Silumesii Maboshe <silumesii@gmail.com>
2025-11-06 13:08:30 +0200

    Add API endpoint to return the environment variables

---
+++ src/pages/api/env.json.ts
@@ -0,0 +1,17 @@
+import type {APIRoute} from "astro";
+
+export const GET: APIRoute = async () => {
+  const envVars = {
+    PUBLIC_TEST_CILANTRO_URL: import.meta.env.PUBLIC_TEST_CILANTRO_URL || "not set",
+    PUBLIC_TEST_OREGANO_URL: import.meta.env.PUBLIC_TEST_OREGANO_URL || "not set",
+    TEST_SECRET_ACCOUNT_ID: import.meta.env.TEST_SECRET_ACCOUNT_ID || "not set",
+    TEST_SECRET_API_KEY: import.meta.env.TEST_SECRET_API_KEY || "not set"
+  };
+
+  return new Response(JSON.stringify(envVars, null, 2), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+};
