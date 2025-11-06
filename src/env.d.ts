178ddcaec4d0ced0e48a59aa1439949095a09a33
Silumesii Maboshe <silumesii@gmail.com>
2025-11-06 12:18:39 +0200

    Tidy up

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,7 @@
 // <reference types="astro/client">
 interface ImportMetaEnv {
   readonly PUBLIC_TEST_CILANTRO_URL: string;
+  readonly PUBLIC_TEST_OREGANO_URL: string;
   readonly TEST_ACCOUNT_ID: string;
   readonly TEST_SECRET_API_KEY: string;
 }
